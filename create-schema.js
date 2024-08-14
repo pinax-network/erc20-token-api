fab8c265b966a293195f5a9b24cb65056fd56f8a
Mathieu Lefebvre <49442766+Matlefebvre1234@users.noreply.github.com>
2024-08-14 13:15:42 -0400

    readd js script

---
+++ create-schema.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+const path = require("path");
+
+// Replace with the actual path to your schema file
+const inputFilePath = path.join(__dirname, "schema.sql.example");
+const outputFilePath = path.join(__dirname, "schema.sql");
+
+// Get the cluster name from the command-line arguments
+const args = process.argv.slice(2);
+const clusterName = args[0]; // First argument after the script name
+
+if (!clusterName) {
+    console.error(
+        "Environment variable 'CLUSTER_NAME' is not set, Cluster and Replicated will be remove",
+    );
+}
+
+// Read the schema file
+fs.readFile(inputFilePath, "utf8", (err, data) => {
+    if (err) {
+        console.error(`Error reading file: ${err}`);
+        process.exit(1);
+    }
+
+    let modifiedData;
+
+    if (clusterName) {
+        // Replace the cluster name in the schema
+        modifiedData = data.replace(
+            /ON CLUSTER\s+\w+/g,
+            `ON CLUSTER ${clusterName}`,
+        );
+
+        console.log(`Cluster name updated to: ${clusterName}`);
+    } else {
+        modifiedData = data.replace(/ON CLUSTER\s+\w+/g, "");
+        modifiedData = modifiedData.replace(/Replicated/g, "");
+    }
+
+    // Write the modified schema to a new file
+    fs.writeFile(outputFilePath, modifiedData, "utf8", (err) => {
+        if (err) {
+            console.error(`Error writing file: ${err}`);
+            process.exit(1);
+        }
+        console.log("Schema updated successfully!");
+    });
+});
